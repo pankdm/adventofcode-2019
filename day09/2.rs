0c63e1a3d6d44d6bf5677a88f57a2220d3a76270
Dmitry Panin <pank.dm@gmail.com>
2019-12-08 21:22:09 -0800

    Day 09

---
+++ day09/2.rs
@@ -0,0 +1,173 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::process;
+
+
+fn get_value(ops: &Vec<i64>, value: i64, mode: i64, base: i64) -> i64 {
+    if mode == 0 {
+        return ops[value as usize];
+    } else if mode == 1 {
+        return value;
+    } else if mode == 2 {
+        return ops[(value + base) as usize];
+    }
+    assert!(false);
+    return -1;
+}
+
+fn get_pos(value: i64, mode: i64, base: i64) -> i64 {
+    let mut pos = 0;
+    if mode == 0 {
+        pos = value
+    } else if mode == 2 {
+        pos = value + base;
+    } else {
+        assert!(false);
+    }
+    return pos;
+}
+
+
+fn process_ops(ops_tmp: Vec<i64>, input: i64) -> Vec<i64> {
+    let mut ops = ops_tmp.clone();
+    let mut res = Vec::new();
+
+    // println!("processing ops: {:?}", ops);
+
+    let mut index = 0;
+    let mut base = 0;
+    while index < ops.len() {
+        let mut value = ops[index];
+        // println!("execute {}", value);
+
+        let op = value % 100;
+        value /= 100;
+
+        let ma = value % 10;
+        value /= 10;
+
+        let mb = value % 10;
+        value /= 10;
+
+        let mc = value % 10;
+
+        if op == 99 {
+            break;
+        } else {
+            if op == 1 {
+                let a = ops[index + 1];
+                let b = ops[index + 2];
+                let c = ops[index + 3];
+                // assert!(mc == 0);
+                let pos = get_pos(c, mc, base);
+                ops[pos as usize] = get_value(&ops, a, ma, base) + get_value(&ops, b, mb, base);
+                index += 4;
+            } else if op == 2 {
+                let a = ops[index + 1];
+                let b = ops[index + 2];
+                let c = ops[index + 3];
+                let pos = get_pos(c, mc, base);
+                ops[pos as usize] = get_value(&ops, a, ma, base) * get_value(&ops, b, mb, base);
+                index += 4;
+            } else if op == 3 {
+                let a = ops[index + 1];
+                // assert!(ma == 0);
+                // ops[a as usize] = get_value(&ops, input, 0);
+                let pos = get_pos(a, ma, base);
+                ops[pos as usize] = input;
+                index += 2;
+            } else if op == 4 {
+                let a = ops[index + 1];
+                let out = get_value(&ops, a, ma, base);
+                res.push(out);
+                println!("   >>> {}", out);
+                index += 2;
+            } else if op == 5 {
+                let a = ops[index + 1];
+                let b = ops[index + 2];
+                if get_value(&ops, a, ma, base) != 0 {
+                    index = get_value(&ops, b, mb, base) as usize;
+                } else {
+                    index += 3;
+                }
+            } else if op == 6 {
+                let a = ops[index + 1];
+                let b = ops[index + 2];
+                if get_value(&ops, a, ma, base) == 0 {
+                    index = get_value(&ops, b, mb, base) as usize;
+                } else {
+                    index += 3;
+                }
+            } else if op == 7 {
+                let a = ops[index + 1];
+                let b = ops[index + 2];
+                let c = ops[index + 3];
+                let pos = get_pos(c, mc, base);
+                if get_value(&ops, a, ma, base) < get_value(&ops, b, mb, base) {
+                    ops[pos as usize] = 1;
+                } else {
+                    ops[pos as usize] = 0;
+                }
+                index += 4;
+
+            } else if op == 8 {
+                let a = ops[index + 1];
+                let b = ops[index + 2];
+                let c = ops[index + 3];
+                // assert!(mc == 0);
+                let pos = get_pos(c, mc, base);
+                if get_value(&ops, a, ma, base) == get_value(&ops, b, mb, base) {
+                    ops[pos as usize] = 1;
+                } else {
+                    ops[pos as usize] = 0;
+                }
+                index += 4;
+            } else if op == 9 {
+                let a = ops[index + 1];
+                base += get_value(&ops, a, ma, base);
+                index += 2;
+            } else {
+                println!("Unknown op: {}", op);
+                assert!(false);
+            }
+        }
+    }
+    return res;
+}
+
+fn main() {
+    // let filename = "t2.txt";
+    let filename = "in.txt";
+
+    let file = File::open(filename).unwrap();
+    let mut reader = BufReader::new(file);
+    let mut line = String::new();
+    reader.read_line(&mut line);
+
+    // println!("read: {}", line);
+
+    let mut str_ops = line.split(",").collect::<Vec<&str>>();
+    // println!("ops: {:?}", ops);
+
+    let mut ops = Vec::new();
+    for str_op in str_ops {
+        ops.push(str_op.parse::<i64>().unwrap());
+    }
+
+    while ops.len() < 10000 {
+        ops.push(0);
+    }
+
+    let INPUT = 2;
+    let res = process_ops(ops.clone(), INPUT);
+
+    if res.len() == 1 {
+        println!("GOOD");
+        println!("ans = {}", res[0]);
+    } else {
+        println!("NOT GOOD");
+        println!("all = {:?}", res);
+    }
+
+
+}
